cdba787eb59897dd935d05af8f6952f0d3430b7b
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-09-28 00:44:19 -0400

    LAB01 Extra Credit

---
+++ LAB01 Extra Credit/LAB01 Extra Credit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB01_Extra_Credit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello world";
+            string name ="Dennis Gaydarzhi";
+            Console.WriteLine($"{greeting} from {name}");
+        }
+    }
+}
