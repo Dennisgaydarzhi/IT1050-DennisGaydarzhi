c20509c7566ea93263f3ea36b0dfd51914dd4a34
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-10-13 00:53:41 -0400

    Add files via upload

---
+++ LAB 04 while loop 10-20/while loop 10-20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loop_10_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+
+            while (number1 <21)
+            {
+                Console.WriteLine(number1);
+                number1++;
+            }
+        }
+    }
+}
