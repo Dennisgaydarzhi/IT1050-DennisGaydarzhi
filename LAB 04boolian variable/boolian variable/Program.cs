8df78fc506ecc413bcb038e19bce7fdd2dc9dec8
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-10-13 00:51:12 -0400

    Add files via upload

---
+++ LAB 04boolian variable/boolian variable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boolian_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isTrue = 61;
+
+            if (isTrue < 60)
+                Console.WriteLine("it is true");
+            else
+                Console.WriteLine("it is false");
+
+        }
+    }
+}
