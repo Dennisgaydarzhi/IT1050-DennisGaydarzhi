3ebf63818464c992e77082923e2bb651e92b43ac
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-10-13 00:48:59 -0400

    Add files via upload

---
+++ LAB 04 speedlimit/speedlimit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace speedlimit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int speedlimit = 35;
+            int speed = 42;
+
+            if (speed > speedlimit)
+            {
+                Console.WriteLine("SLOW DOWN");
+            }
+        }
+    }
+}
