5d012f7388cecc1487c7ae44627bc430bf6c0a82
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-10-13 00:43:29 -0400

    Add files via upload

---
+++ F to C/F to C/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F_to_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double celsius;
+            Double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            if(fahrenheit < 40)
+            {
+                Console.WriteLine("It is cold");
+            }
+            else
+                Console.WriteLine("It is hot");
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine($"celsius {celsius}");
+        }
+    }
+}
