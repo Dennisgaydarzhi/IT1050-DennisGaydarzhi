e166190f7d9321294794436abfe02e93c88fdab2
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-10-13 00:53:06 -0400

    Add files via upload

---
+++ LAB 04 while loop --/while loop --/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loop___
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 60;
+
+            while (number1 > 19)
+            {
+                Console.WriteLine(number1);
+                number1--;
+            }
+        }
+    }
+}
