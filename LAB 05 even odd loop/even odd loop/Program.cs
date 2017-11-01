078dbf2086a618913af0fc759a0c2e366c0d4ad8
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-10-31 20:26:59 -0400

    Add files via upload

---
+++ LAB 05 even odd loop/even odd loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_odd_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i<101; i++)
+            {
+                if ((i % 2) == 0)
+                    Console.WriteLine($"{i} its even");
+
+                else if ((i % 2) != 0)
+                    Console.WriteLine($" {i} its odd");
+            }
+        }
+    }
+}
