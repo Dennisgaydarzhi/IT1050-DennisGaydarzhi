c03a4c1ba33fdcf24382087a88f29e33960f22bb
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-10-31 22:46:16 -0400

    Add files via upload

---
+++ LAB 05 Loop 10-20 correction/LAB 05 Loop 10-20 correction/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_05_Loop_10_20_correction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            while (i < 21)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
