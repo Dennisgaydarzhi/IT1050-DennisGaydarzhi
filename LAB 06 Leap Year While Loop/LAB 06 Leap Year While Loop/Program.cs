5f91a214f8ada3849d6dad0c39dde4f3c6e55532
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-11-05 14:35:33 -0500

    Add files via upload

---
+++ LAB 06 Leap Year While Loop/LAB 06 Leap Year While Loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_06_Leap_Year_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int year = 2017; year <= 2100; year++)
+            {
+                if (DateTime.IsLeapYear(year))
+                {
+                    Console.WriteLine("{0} is a leap year.", year);
+                    DateTime leapDay = new DateTime(year, 2, 29);
+                }
+            }
+
+        }
+        
+    }
+}
