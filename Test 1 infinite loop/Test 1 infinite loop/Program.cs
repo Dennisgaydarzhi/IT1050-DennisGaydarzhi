776b6fef5f5f0833f6921a2db28b48f035c9fd08
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-11-12 17:15:34 -0500

    Add files via upload

---
+++ Test 1 infinite loop/Test 1 infinite loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1_infinite_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keeplooping = true;
+
+            do
+            {
+                Console.WriteLine("true");
+
+            } while (keeplooping == true);
+               
+        }
+    }
+}
