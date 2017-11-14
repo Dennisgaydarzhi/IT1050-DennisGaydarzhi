551dd4bf7d0d19c7a07de737db798d2f1a124260
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-11-13 20:29:07 -0500

    Add files via upload

---
+++ Test 1 2-128 loop/Test 1 2-128 loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1_2_128_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            while (x <= 128)
+            {
+                Console.WriteLine("{" + x + "]");
+                x * 2;
+
+            }
+        }
+    }
+}
