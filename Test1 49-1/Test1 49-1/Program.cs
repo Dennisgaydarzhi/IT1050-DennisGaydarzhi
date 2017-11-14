78c1c49f3171941d7d78c8e572e21df105b6293c
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-11-13 20:41:44 -0500

    Add files via upload

---
+++ Test1 49-1/Test1 49-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_49_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            for (x = 49; x > 0;)
+            {
+                Console.Write("{0},", x);
+                x--;
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
