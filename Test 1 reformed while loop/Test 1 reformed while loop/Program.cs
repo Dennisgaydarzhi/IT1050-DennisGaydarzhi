e22ae9548d3be1749c62f7af910f56dc0c1b87ed
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-11-13 21:14:48 -0500

    Add files via upload

---
+++ Test 1 reformed while loop/Test 1 reformed while loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1_reformed_while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8;
+            int i = 10;
+            while (i < n)
+            {
+                Console.Write("*");
+                i++;
+            }
+            // the do while loop has one * while the while loop does not
+        }
+
+    }
+}
