1894299a8b243ed93ee6e6d32ba8d880e2e278ea
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-11-13 20:47:50 -0500

    Add files via upload

---
+++ Test1 1-21 odds/Test1 1-21 odds/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_1_21_odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+
+            while (x <= 21)
+            {
+                Console.Write(x+"   ");
+                x++;x++;
+            }
+        }
+    }
+}
