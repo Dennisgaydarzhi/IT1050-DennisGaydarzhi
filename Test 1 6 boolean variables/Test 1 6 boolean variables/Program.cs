ca5a68a18d5212fd84fbf48e7796ef06ccd11617
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-11-13 21:34:07 -0500

    Add files via upload

---
+++ Test 1 6 boolean variables/Test 1 6 boolean variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1_6_boolean_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool icyRain = false;
+            bool tornadoWarning = false;
+           if ( icyRain || tornadoWarning == false)
+            {
+                Console.WriteLine("Lets go outside!");
+            }
+        }
+    }
+}
