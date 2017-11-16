0f94e7a67a4540a88a04137a9e7cd5844efe088a
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-11-16 10:42:45 -0500

    Add files via upload

---
+++ LAB 07 MyMath/MyMath/Mymath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+    public class MyMath
+    {
+        public double operand1;
+        public double operand2;
+        public static double Result = 0;
+        static public void Multiply(double operand1, double operand2)
+        {
+            Result = operand1 * operand2;
+            Console.WriteLine(operand1+ "*"+operand2+"=");
+        }
+        static public void Divide(double operand1, double operand2)
+        {
+            Result = operand1 / operand2;
+            Console.WriteLine(operand1 + "/" + operand2 + "=");
+        }
+        static public void Subtract(double operand1, double operand2)
+        {
+            Result = operand1 - operand2;
+            Console.WriteLine(operand1 + "-" + operand2 + "=");
+        }
+        static public void Add(double operand1, double operand2)
+        {
+            Result = operand1 + operand2;
+            Console.WriteLine(operand1 + "+" + operand2+"=");
+        }
+        static public void GetResult()
+        {
+            Console.WriteLine(Result);
+        }
+    }
+}
---
+++ LAB 07 MyMath/MyMath/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+    class Program:MyMath
+    {
+        static void Main(string[] args)
+        {
+            
+            double x = 10;
+            double y = 10;
+
+            Multiply(x, y);
+            GetResult();
+
+            Divide(x, y);
+            GetResult();
+
+            Subtract(x, y);
+            GetResult();
+
+            Add(x, y);
+            GetResult();
+        
+        }
+
+        
+    }
+}
