3b239226501890acb6f2dd8ac0c0012b2b6de5e8
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-11-26 18:35:51 -0500

    Add files via upload

---
+++ LAB 08 3/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] seasons = new string[4];
+
+            seasons[0] = "Winter";
+            seasons[1] = "Spring";
+            seasons[2] = "Summer";
+            seasons[3] = "Fall";
+
+            foreach (string season in seasons)
+            {
+                Console.Write(season + ","); 
+
+            }
+
+        }
+    }
+}
