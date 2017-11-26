a111508b028603eb33479e19f78f36e4decd6fac
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-11-26 18:47:38 -0500

    Add files via upload

---
+++ LAB 08 4/LAB 08 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_08_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomnumber = new int[1000];
+            Random random = new Random();
+            for (int i = 0; i < randomnumber.Length; i++)
+            {
+                randomnumber[i] = random.Next(0, 100);
+            }
+            foreach (int i in randomnumber)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
+
