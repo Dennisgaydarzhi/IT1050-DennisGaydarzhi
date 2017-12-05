4d6ac8ef1e1d9099870f24db7d2a5f1bf950a7ee
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-12-04 23:43:23 -0500

    Add files via upload

---
+++ LAB 09 1/LAB 09 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_09_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fruits = new[] { "apple", "mango", "orange", "apricot", "cherry", "grape", "blueberry" };
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+
+    }
+}
