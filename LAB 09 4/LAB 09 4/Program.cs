c3aa90245ca6f4b6a7a0b5617155dc9bb4584f0e
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-12-06 21:57:28 -0500

    Add files via upload

---
+++ LAB 09 4/LAB 09 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_09_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryStringList();
+        }
+        static void QueryStringList()
+        {
+            int count = 0;
+            List<string> T = new List<string>();
+
+            T.Capacity = 6;
+            T.Add("January");
+            T.Add("Febuary");
+            T.Add("March");
+            T.Add("April");
+            T.Add("May");
+            T.Add("June");
+
+            var TSpaces = from i in T
+                          where i.EndsWith("ber")
+                          select i;
+
+            foreach (string i in TSpaces)
+            {
+                Console.WriteLine(i);
+                count++;
+            }
+
+            Console.WriteLine("Count:" + count);
+            Console.WriteLine("Capacity:" + T.Capacity);
+
+        }
+
+
+    }
+}
