c2fed8fb4975008ed8cd4436030efa987cea568b
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-12-06 21:56:33 -0500

    Add files via upload

---
+++ LAB 09 2/LAB 09 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_09_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryStringArray();
+        }
+          
+        static void QueryStringArray()
+        {
+            var fruits = new[] { "apple", "mango", "orange", "apricot", "cherry", "grape", "blueberry" };
+
+            var Uppercase = from fruit in fruits
+                               where fruit.StartsWith("a")
+
+                               select fruit.ToUpper();
+
+            foreach(var fruit in Uppercase)
+            {
+                Console.WriteLine(fruit);
+            }
+
+
+
+        }
+    }
+}
