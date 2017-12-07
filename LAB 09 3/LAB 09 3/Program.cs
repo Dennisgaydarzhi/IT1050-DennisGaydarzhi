8646ad083c57fc747af901f8ea2019ae6527d5ef
Dennisgaydarzhi <32319516+Dennisgaydarzhi@users.noreply.github.com>
2017-12-06 21:57:03 -0500

    Add files via upload

---
+++ LAB 09 3/LAB 09 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_09_3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+            List<string> T = new List<string>();
+
+            T.Capacity = 6;
+            T.Add("January");
+            T.Add("Febuary");
+            T.Add("March");
+            T.Add("April");
+            T.Add("May");
+            T.Add("June");
+            T.Add("july");
+            T.Add("August");
+            T.Add("Spetember");
+            T.Add("October");
+            T.Add("November");
+            T.Add("December");
+
+            foreach (string i in T)
+            {
+                Console.WriteLine(i);
+                count++;
+            }
+            Console.WriteLine("Count:" + count);
+            Console.WriteLine("Capacity:" + T.Capacity);
+        } 
+    }
+}
